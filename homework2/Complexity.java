592eb3bb02ae0915e1a85bd4bebd14107b234872
jschmidtnj <joshuanschmidt@gmail.com>
2018-09-15 12:00:34 -0400

    finished for the most part hw2

---
+++ homework2/Complexity.java
@@ -0,0 +1,73 @@
+package homework2;
+
+public class Complexity {
+	public static void method0(int n) {
+		// O(n) complexity
+		int counter = 0;
+		for (int i = 0; i < n; i++) {
+			System.out.println(" Operation " + counter);
+			counter++;
+		}
+	}
+
+	public static void method1(int n) {
+		// O(n^2) complexity
+		int counter = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.println(" Operation " + counter);
+				counter++;
+			}
+		}
+	}
+
+	public static void method2(int n) {
+		// O(log(n)) complexity
+		int counter = 0;
+		for (int i = 1; i < n; i *= 2) {
+			System.out.println(" Operation " + counter);
+			counter++;
+		}
+	}
+
+	public static void method3(int n) {
+		// O(n*log(n)) complexity
+		int counter = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < n; j *= 2) {
+				System.out.println(" Operation " + counter);
+				counter++;
+			}
+		}
+	}
+
+	public static void method4(int n) {
+		// O(n^3) complexity
+		int counter = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					System.out.println(" Operation " + counter);
+					counter++;
+				}
+			}
+		}
+	}
+	
+	public static void method5(int n) {
+		// O(2^n) complexity
+		int counter = 0;
+		for (int i = 1; i < n; i *= counter) {
+			System.out.println(" Operation " + counter);
+			counter++;
+		}
+	}
+	
+	public static int method6(int n) {
+		if (n <= 0) {
+			return 0;
+		} else {
+			return 1 + method6(n-1) + method6(n-1);
+		}
+	}
+}
---
+++ homework2/TestComplexity.java
@@ -0,0 +1,86 @@
+package homework2;
+
+import java.lang.Math;
+
+public class TestComplexity {
+	public static void main(String[] args) {
+		int pausedelay = 500; // milliseconds
+		System.out.println("method 0");
+		try {
+			Thread.sleep(pausedelay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int nummeth0 = 3;
+		Complexity.method0(nummeth0);
+		System.out.print("n for method 0: ");
+		System.out.println(nummeth0);
+		System.out.println("method 1");
+		try {
+			Thread.sleep(pausedelay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int nummeth1 = 3;
+		Complexity.method1(nummeth1);
+		System.out.print("n^2 for method 1: ");
+		System.out.println(Math.pow(nummeth1, 2));
+		System.out.println("method 2");
+		try {
+			Thread.sleep(pausedelay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int nummeth2 = 15;
+		Complexity.method2(nummeth2);
+		System.out.print("log(n) for method 2: ");
+		System.out.println(Math.log(nummeth2) / Math.log(2));
+		System.out.println("method 3");
+		try {
+			Thread.sleep(pausedelay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int nummeth3 = 10;
+		Complexity.method3(nummeth3);
+		System.out.print("n*log(n) for method 3: ");
+		System.out.println(nummeth3 * Math.log(nummeth3) / Math.log(2));
+		System.out.println("method 4");
+		try {
+			Thread.sleep(pausedelay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int nummeth4 = 3;
+		Complexity.method4(nummeth4);
+		System.out.print("n^3 for method 4: ");
+		System.out.println(Math.pow(nummeth4, 3));
+		System.out.println("method 5");
+		try {
+			Thread.sleep(pausedelay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int nummeth5 = 3;
+		Complexity.method5(nummeth5);
+		System.out.print("log(log(n)) for method 5: ");
+		System.out.println(Math.log(Math.log(nummeth5) / Math.log(2)) / Math.log(2));
+		System.out.println("method 6");
+		try {
+			Thread.sleep(pausedelay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int nummeth6 = 10;
+		System.out.println(Complexity.method6(nummeth6));
+		System.out.print("2^n for method 6: ");
+		System.out.println(Math.pow(2, nummeth6));
+	}
+}
