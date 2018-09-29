91a355e37c2ba6ee72edd481a7e101412eb78397
Joshua Schmidt <jschmidtnj@gmail.com>
2018-09-29 17:14:46 -0400

    added junit test cases instead of print statements

--- homework3/IDLListTest.java
+++ homework3/IDLListTest.java
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class IDLListTest {
 
-	IDLList<Integer> l1 = new IDLList<Integer>();
 	@Test
 	void test() {
-		l1.add(10);
-		assertEquals(l1.toString(), "[10]");
-		assertEquals(true, true);
+		IDLList<Integer> l1 = new IDLList<Integer>();
+		l1.add(3);
+		assertEquals(l1.toString(), "[3]");
+		l1.add(4);
+		assertEquals(l1.toString(), "[4,3]");
+		assertEquals(l1.size(), 2);
+		assertEquals(l1.remove() == 4, true);
+		assertEquals(l1.getHead() == 3, true);
+		assertEquals(l1.getLast() == 3, true);
 	}
 
 }
